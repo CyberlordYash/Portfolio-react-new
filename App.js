d008d6e9bc13365d52d97fa673d4acd4538fe2f2
Yash Sachan <yashsachan321@gmail.com>
2023-06-09 12:15:39 +0530

    1.added animated lines to separate pages 2.contact page dark mode enabled

--- App.js
+++ App.js
@@ -8,15 +8,23 @@ import Skills from "./components/Skills/Skills";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Footer from "./components/Footer/Footer";
 import DarkMode from "./DarkMode";
+import Lottie from "lottie-react";
+import Line from "./assets/line.json";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+
       <Header></Header>
+      <Lottie animationData={Line} className="line"></Lottie>
       <About></About>
+      <Lottie animationData={Line} className="line"></Lottie>
       <Work></Work>
+      <Lottie animationData={Line} className="line"></Lottie>
       <Skills></Skills>
+      <Lottie animationData={Line} className="line"></Lottie>
       <Testimonials></Testimonials>
+      <Lottie animationData={Line} className="line"></Lottie>
       <Footer></Footer>
 
       <h1>Yash Sachan</h1>
