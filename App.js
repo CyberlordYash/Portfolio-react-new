6c06fe3de2f3bf998ba1e74e457ac1dc47c97bf9
Yash Sachan <yashsachan321@gmail.com>
2023-06-09 14:31:38 +0530

    added loading screen

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./app.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
@@ -10,25 +10,39 @@ import Footer from "./components/Footer/Footer";
 import DarkMode from "./DarkMode";
 import Lottie from "lottie-react";
 import Line from "./assets/line.json";
+import Loading from "./assets/loading.json";
+import Github from "./assets/github.json";
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
   return (
     <div className="app">
-      <Navbar />
+      {loading ? (
+        <div className="loading">
+          <Lottie animationData={Loading} className="loadin"></Lottie>
+        </div>
+      ) : (
+        <div>
+          <Navbar />
 
-      <Header></Header>
-      <Lottie animationData={Line} className="line"></Lottie>
-      <About></About>
-      <Lottie animationData={Line} className="line"></Lottie>
-      <Work></Work>
-      <Lottie animationData={Line} className="line"></Lottie>
-      <Skills></Skills>
-      <Lottie animationData={Line} className="line"></Lottie>
-      <Testimonials></Testimonials>
-      <Lottie animationData={Line} className="line"></Lottie>
-      <Footer></Footer>
-
-      <h1>Yash Sachan</h1>
-      <p>yash</p>
+          <Header></Header>
+          <Lottie animationData={Line} className="line"></Lottie>
+          <About></About>
+          <Lottie animationData={Line} className="line"></Lottie>
+          <Work></Work>
+          <Lottie animationData={Line} className="line"></Lottie>
+          <Skills></Skills>
+          <Lottie animationData={Line} className="line"></Lottie>
+          <Testimonials></Testimonials>
+          <Lottie animationData={Line} className="line"></Lottie>
+          <Footer></Footer>
+        </div>
+      )}
     </div>
   );
 };
