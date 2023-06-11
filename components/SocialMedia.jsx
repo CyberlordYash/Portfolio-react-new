6ebd05651b5cc121bd6906fddb2d6c01b921a5f6
Yash Sachan <yashsachan321@gmail.com>
2023-06-11 23:30:44 +0530

    revamped socialMedia UI , minor UI imporovements

--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { BsGithub, BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <a href="https://twitter.com/cyberlordyash?t=ctKn3D5kvlO6CunzNKopHg&s=08">
-        <div>
+        <div className="twitter">
           <BsTwitter></BsTwitter>
         </div>
       </a>
-      <div>
-        <FaFacebookF></FaFacebookF>
+      <div className="github">
+        <BsGithub></BsGithub>
       </div>
       <a href="https://instagram.com/isthat_yash__?igshid=MzNlNGNkZWQ4Mg==">
-        <div>
+        <div className="insta">
           <BsInstagram></BsInstagram>
         </div>
       </a>
